1a637e1b829381ef2ff2c294673c5e9d51e81d01
315870 <60711142+315870@users.noreply.github.com>
2020-05-23 15:26:31 -0400

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HangmanProject
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+        
+    public partial class MainWindow : Window
+    {
+        string guessWord;
+        string[] words; 
+        Random r = new Random(1);
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+            try
+            {
+                System.IO.StreamReader sr = new System.IO.StreamReader("words.txt");
+                words = new string[10];
+                int counter = 0;
+                while (!sr.EndOfStream)
+                {
+                    words[counter] = sr.ReadLine();
+                    // MessageBox.Show(words[counter]);
+                    //Console.WriteLine(words[counter] + " " + words[counter].Length.ToString());
+                    counter++;
+                    
+                    // MessageBox.Show(r.Next(10).ToString());
+
+                    
+
+
+                }
+                sr.Close();
+            }
+
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            guessWord = words[r.Next(10)];
+            Console.WriteLine(guessWord);
+            string buildWord = "";
+            for (int i=0; i < guessWord.Length; i++)
+            {
+                buildWord += "_ ";
+            }
+            Console.WriteLine(buildWord);
+            string guessedLetter = "d";
+
+            for (int i =0; i < guessWord.Length; i++)
+            {
+                if(guessedLetter == guessWord[i].ToString())
+                {
+                    buildWord += guessedLetter + " ";
+                }
+               // MessageBox.Show(guessWord[i].ToString());
+               else
+                {
+                    buildWord += guessedLetter[i*2] + " ";
+                }
+
+            }
+            lblwordHint.Content = buildWord;
+
+
+
+
+        }
+
+        
+    }
+}
